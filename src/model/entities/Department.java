9048656fd51fb3709988763bdb0b160425e1541e
JohnnatanTomaz <johnnatan.tomaz1@gmail.com>
2021-07-15 12:35:07 -0300

    Department List Controller

---
+++ src/model/entities/Department.java
@@ -0,0 +1,58 @@
+package model.entities;
+
+import java.util.Objects;
+
+public class Department {
+
+    private Integer id;
+    private String name;
+
+    public Department() {
+    }
+
+    public Department(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Department that = (Department) o;
+        return Objects.equals(id, that.id) && Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Department [ID = "
+                + id
+                + ", Name ="
+                + name
+                + "]";
+    }
+}
